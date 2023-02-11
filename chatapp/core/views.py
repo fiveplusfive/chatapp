57af5cdeab18445f65e0f2616466187703571305
fiveplusfive <125126987+fiveplusfive@users.noreply.github.com>
2023-02-11 16:21:05 +0530

    Add files via upload

---
+++ chatapp/core/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+from django.contrib.auth import login
+
+from .forms import SignUpForm
+
+def frontpage(request):
+    return render(request, 'core/frontpage.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+
+            return redirect('frontpage')
+    else:
+        form = SignUpForm()
+    
+    return render(request, 'core/signup.html', {'form': form})
